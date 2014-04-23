b7a9016c3b4495eaca55e69f07fb7db31ebfb625
Gary Kenneally <garykenneally@gmail.com>
2014-04-23 22:49:33 +0100

    GK - Added Byte extensions

--- Eloquent.Tests/IntExtensionsTests.cs
+++ Eloquent.Tests/IntExtensionsTests.cs
@@ -20,5 +20,82 @@ namespace Eloquent.Tests
                 Assert.IsTrue(list.All(x => x.Equals("Item")));
             }
         }
+
+        [TestClass]
+        public class Bytes
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(10.Bytes(), 4.Bytes() + 6.Bytes());
+            }
+        }
+
+        [TestClass]
+        public class Kilobytes
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(10.Kilobytes(), 4.Kilobytes() + 6.Kilobytes());
+            }
+        }
+
+        [TestClass]
+        public class Megabytes
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(10.Megabytes(), 4.Megabytes() + 6.Megabytes());
+            }
+        }
+
+        [TestClass]
+        public class Gigabytes
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(10.Gigabytes(), 4.Gigabytes() + 6.Gigabytes());
+            }
+        }
+
+        [TestClass]
+        public class Terabytes
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(10.Terabytes(), 4.Terabytes() + 6.Terabytes());
+            }
+        }
+
+        [TestClass]
+        public class Petabytes
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(10.Petabytes(), 4.Petabytes() + 6.Petabytes());
+            }
+        }
+
+        [TestClass]
+        public class Exabytes
+        {
+            [TestMethod]
+            public void Can_Add_Exabytes()
+            {
+                Assert.AreEqual(10.Exabytes(), 4.Exabytes() + 6.Exabytes());
+            }
+
+            [TestMethod]
+            public void Can_Add_Exabytes_All()
+            {
+                var result = 1.Exabytes() + 1.Petabytes() + 1.Terabytes() + 1.Gigabytes() + 1.Megabytes() + 1.Kilobytes() + 1.Bytes();
+                Assert.AreEqual(1154048505100108801, result);
+            }
+        }
     }
 }
--- Eloquent/IntExtension.cs
+++ Eloquent/IntExtension.cs
@@ -4,6 +4,48 @@ namespace Eloquent
 {
     public static class IntExtensions
     {
+        private const Int64 KILOBYTE = 1024;
+        private const Int64 MEGABYTE = KILOBYTE * 1024;
+        private const Int64 GIGABYTE = MEGABYTE * 1024;
+        private const Int64 TERABYTE = GIGABYTE * 1024;
+        private const Int64 PETABYTE = TERABYTE * 1024;
+        private const Int64 EXABYTE =  PETABYTE * 1024;
+
+        public static Int64 Bytes(this int number)
+        {
+            return number;
+        }
+
+        public static Int64 Kilobytes(this int number)
+        {
+            return number * KILOBYTE;
+        }
+
+        public static Int64 Megabytes(this int number)
+        {
+            return number * MEGABYTE;
+        }
+
+        public static Int64 Gigabytes(this int number)
+        {
+            return number * GIGABYTE;
+        }
+
+        public static Int64 Terabytes(this int number)
+        {
+            return number * TERABYTE;
+        }
+
+        public static Int64 Petabytes(this int number)
+        {
+            return number * PETABYTE;
+        }
+
+        public static Int64 Exabytes(this int number)
+        {
+            return number * EXABYTE;
+        }
+
         public static void Times(this int times, Action action)
         {
             for (int i = 0; i < times; i++)
