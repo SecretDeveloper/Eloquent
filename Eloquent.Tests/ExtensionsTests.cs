37e4931fbda7b88755e43bb1422276e1786d2d67
Gary Kenneally <garykenneally@gmail.com>
2014-04-23 18:42:18 +0100

    Initial checkin with a few Extension methods

---
+++ Eloquent.Tests/ExtensionsTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Eloquent.Tests
+{
+    public class ExtensionsTests
+    {
+        [TestClass]
+        public class Times
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                var list = new List<string>();
+                10.Times(() => list.Add("Item"));
+
+                Assert.AreEqual(10, list.Count);
+                Assert.IsTrue(list.All(x=>x.Equals("Item")));
+            }
+        }
+
+        [TestClass]
+        public class Milliseconds
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(10, 10.Milliseconds().Milliseconds);
+            }
+        }
+
+        [TestClass]
+        public class Seconds
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(10, 10.Seconds().Seconds);
+            }
+        }
+
+        [TestClass]
+        public class Minutes
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(10, 10.Minutes().Minutes);
+            }
+        }
+
+        [TestClass]
+        public class Hours
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(10, 10.Hours().Hours);
+            }
+        }
+
+        [TestClass]
+        public class Days
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(10, 10.Days().Days);
+            }
+        }
+
+        [TestClass]
+        public class Ago
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(DateTime.Now.Subtract(10.Seconds()).ToShortTimeString(), 10.Seconds().Ago().ToShortTimeString());
+            }
+        }
+
+        [TestClass]
+        public class FromNow
+        {
+            [TestMethod]
+            public void Can_Count()
+            {
+                Assert.AreEqual(DateTime.Now.Add(10.Seconds()).ToShortTimeString(), 10.Seconds().FromNow().ToShortTimeString());
+            }
+        }
+    }
+}
---
+++ Eloquent/Extensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eloquent
+{
+    /*
+     * Eloquent Expressions
+     * 
+     * 10.Minutes.Ago
+     * 10.Years
+     * 10.Times(x=>x...)
+     * DateTime dt = 10.Days.FromNow
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+
+    public static class Extensions
+    {
+        public static void Times(this int times, Action action)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                action();
+            }
+        }
+
+        public static TimeSpan Milliseconds(this int value)
+        {
+            return new TimeSpan(0, 0, 0, 0, value);
+        }
+
+        public static TimeSpan Seconds(this int value)
+        {
+            return new TimeSpan(0, 0, 0, value);
+        }
+
+        public static TimeSpan Minutes(this int value)
+        {
+            return new TimeSpan(0, 0, value, 0);
+        }
+
+        public static TimeSpan Hours(this int value)
+        {
+            return new TimeSpan(0, value, 0, 0);
+        }
+
+        public static TimeSpan Days(this int value)
+        {
+            return new TimeSpan(value, 0, 0, 0);
+        }
+
+        public static DateTime Ago(this TimeSpan value)
+        {
+            return DateTime.Now.Subtract(value);
+        }
+
+        public static DateTime FromNow(this TimeSpan value)
+        {
+            return DateTime.Now.Add(value);
+        }
+    }
+}
